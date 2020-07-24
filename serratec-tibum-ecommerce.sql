5744a50233e595b6837a78868d77d9ba22036819
Jonathan <nogueira.jonathan@gmail.com>
2020-07-24 16:06:49 -0300

    Tabela Pedidos_Produtos

--- serratec-tibum-ecommerce.sql
+++ serratec-tibum-ecommerce.sql
@@ -1,32 +1,7 @@
-Create Database Tibum_Store
-go
 
-Use Tibum_Store
-
-Create Table Cliente (
-    id_Cliente int identity primary key,
-    Nome varchar(80) not null,
-    Endereco varchar(120) not null,
-    ClienteLogin varchar (20) not null unique,
-    Senha varchar(30) not null,
-    Email char (80) not null unique,
-    CPF char (11) not null unique,
-    Data_Nasc Date not null,
-    Telefone char (11)
-)
-
-Create Table Produtos (
-    id_Produto int identity primary key,
-    NomeProduto varchar (20) not null,
-    QtdEstoque int not null,
-    DataFabric Date,
-    Valor float not null,
-    DataCadastro Date Default GetDate()
-)
-
-Create Table Funcionarios (
-    Salario varchar(20) not null,
-    Codigo int identity primary key,
-    Nome varchar (20) not null,
-    CPF char (11) not null
-)
+Create Table Pedido_Produtos (
+	id_Produto int, 
+	id_Pedido int,
+	foreign key (id_Produto) references Produtos (id_Produto),
+	foreign key (id_Pedido) references Pedido (id_Pedido)
+	)
