0ec787d4dbe576c6cd6a42053310ba7fca251212
Jonathan <nogueira.jonathan@gmail.com>
2020-07-24 14:24:24 -0300

    Tabela Produto

--- serratec-tibum-ecommerce.sql
+++ serratec-tibum-ecommerce.sql
@@ -7,7 +7,7 @@ Use Tibum_Store
 Create Table Cliente (
 id_Cliente int identity Primary Key,
 Nome varchar(80) not null,
-Endereço varchar(120) not null,
+Endereco varchar(120) not null,
 ClienteLogin varchar (20) not null,
 Senha int not null,
 Email char (80) not null,
@@ -15,4 +15,17 @@ CPF char (14) not null,
 Data_Nasc Date not null,
 Telefone int,
 )
+
+Create Table Produtos (
+id_Produto char (120) not null,
+NomeProduto varchar (20) not null,
+QtdEstoque int identity (1,1) not null,
+DataFabric Date,
+Valor int not null,
+DataCadastro Date GetDate(),
+)
+
+
+
+
 
