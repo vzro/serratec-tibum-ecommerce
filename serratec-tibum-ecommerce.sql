96de0ea9bcd43fb473f7014c94e4f52c39d6786a
Jonathan <nogueira.jonathan@gmail.com>
2020-07-24 14:17:36 -0300

    Tabela Cliente

--- serratec-tibum-ecommerce.sql
+++ serratec-tibum-ecommerce.sql
@@ -1,1 +1,18 @@
 -- arquivo inicial
+Create Database Tibum_Store
+go
+
+Use Tibum_Store
+
+Create Table Cliente (
+id_Cliente int identity Primary Key,
+Nome varchar(80) not null,
+Endereço varchar(120) not null,
+ClienteLogin varchar (20) not null,
+Senha int not null,
+Email char (80) not null,
+CPF char (14) not null,
+Data_Nasc Date not null,
+Telefone int,
+)
+
